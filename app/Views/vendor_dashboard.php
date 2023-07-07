c95fcaab780f29f7afbed41964d0fd300ff78b9d
Humphrey Dulo <106704855+HumphreyDulo@users.noreply.github.com>
2023-07-07 18:50:39 +0300

    incorporating LTE page with approval or denial

--- app/Views/vendor_dashboard.php
+++ app/Views/vendor_dashboard.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Dashboard</title>
+    <title>Vendor_Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
